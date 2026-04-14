5c34c7c10e5853b3710763226e5f0b5bfe829377
galacticgadgetry <galactic-gadgetry@gmail.com>
2026-04-14 09:55:20 -0400

    Add License Item list on Dashboard view

---
+++ LicenseTracker/Models/LicenseItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LicenseTracker.Models
+{
+    public enum LicenseStatus
+    {
+        Active,
+        Archived,
+        Expired,
+        Inactive,
+    }
+
+
+
+    class LicenseItem
+    {
+
+        public DateTime ExpirationDate { get; set; }
+
+
+        public DateTime IssueDate { get; set; }
+
+
+        public string LicenseId { get; set; }
+
+
+        public string Product { get; set; }
+
+
+        public LicenseStatus Status { get; set; }
+
+
+        public string User { get; set; }
+    }
+}
--- LicenseTracker/ViewModels/DashboardViewModel.cs
+++ LicenseTracker/ViewModels/DashboardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LicenseTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,12 @@ namespace LicenseTracker.ViewModels
 {
     internal class DashboardViewModel : ViewModelBase
     {
+
+
+        public List<LicenseItem> Licenses { get; set; } = new()
+        {
+            new LicenseItem { ExpirationDate = DateTime.Now.AddYears(1), IssueDate = DateTime.Now, LicenseId = "1234567890", Product = "Mathematica", Status = LicenseStatus.Active, User = "User Name 1" },
+            new LicenseItem { ExpirationDate = DateTime.Now.AddYears(2), IssueDate = DateTime.Now, LicenseId = "0987654321", Product = "MATLAB", Status = LicenseStatus.Archived, User = "User Name 2" }
+        };
     }
 }
