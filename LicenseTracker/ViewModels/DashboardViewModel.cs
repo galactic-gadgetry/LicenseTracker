45406abc49d1d3033cbdd3b80ad43eac531a1262
galacticgadgetry <galactic-gadgetry@gmail.com>
2026-04-14 10:59:04 -0400

    Add Tool Bar button command on Dashboard view

--- LicenseTracker/ViewModels/DashboardViewModel.cs
+++ LicenseTracker/ViewModels/DashboardViewModel.cs
@@ -1,7 +1,9 @@
-﻿using LicenseTracker.Models;
+﻿using LicenseTracker.Commands;
+using LicenseTracker.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace LicenseTracker.ViewModels
 {
@@ -14,5 +16,24 @@ namespace LicenseTracker.ViewModels
             new LicenseItem { ExpirationDate = DateTime.Now.AddYears(1), IssueDate = DateTime.Now, LicenseId = "1234567890", Product = "Mathematica", Status = LicenseStatus.Active, User = "User Name 1" },
             new LicenseItem { ExpirationDate = DateTime.Now.AddYears(2), IssueDate = DateTime.Now, LicenseId = "0987654321", Product = "MATLAB", Status = LicenseStatus.Archived, User = "User Name 2" }
         };
+
+
+
+        public ICommand NewLicenseButtonClickedCommand { get; }
+
+
+
+        public DashboardViewModel()
+        {
+            NewLicenseButtonClickedCommand = new RelayCommand(
+                new Action<object?>(OnNewLicenseButtonClicked));
+        }
+
+
+
+        private void OnNewLicenseButtonClicked(object? obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
