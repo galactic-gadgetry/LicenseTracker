4bf8e7d84b3348d4bbac0c28b5a2a50c3971d2b6
galacticgadgetry <galactic-gadgetry@gmail.com>
2026-04-23 15:02:48 -0400

    WIP new license functionality

---
+++ LicenseTracker/UIComponents/Dialogs/CreateNewLicenseDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LicenseTracker.UIComponents.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for CreateNewLicenseDialog.xaml
+    /// </summary>
+    public partial class CreateNewLicenseDialog : Window
+    {
+        public CreateNewLicenseDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
